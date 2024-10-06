80222c39865d30a8791acbf1eb59b7cce9ebc247
Ivan Tsenilov <arantirex@gmail.com>
2024-10-06 17:15:24 +0400

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises";
+import path from "path";
+import { getCurrentDir } from "../utils/utils.js";
+
 const list = async () => {
-    // Write your code here 
+  const { __dirname } = getCurrentDir(import.meta.url);
+  const filesPath = path.join(__dirname, "files");
+
+  try {
+    const files = await fs.readdir(filesPath);
+    for (const file of files) console.log(file);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
