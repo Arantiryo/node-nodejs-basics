ec93371dd24155960b04e641293c0079a647aa2e
Ivan Tsenilov <arantirex@gmail.com>
2024-10-06 16:32:51 +0400

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import fs from "node:fs/promises";
+import path from "path";
+import { getCurrentDir } from "../utils/utils.js";
+
 const copy = async () => {
-    // Write your code here 
+  const { __dirname } = getCurrentDir(import.meta.url);
+
+  const srcFolder = path.join(__dirname, "files");
+  const destFolder = path.join(__dirname, "files_copy");
+
+  try {
+    await fs.cp(srcFolder, destFolder, {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
