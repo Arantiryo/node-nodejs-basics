e5f5c34eb86439a6b8567a7227e8d47f80aece36
Ivan Tsenilov <arantirex@gmail.com>
2024-10-07 10:45:49 +0400

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+  const parsedArgs = process.argv
+    .slice(2)
+    .reduce((acc, val, idx, arr) => {
+      if (val.startsWith("--")) {
+        const argument = val.slice(2);
+        const value = arr?.[idx + 1];
+
+        acc.push(`${argument} is ${value}`);
+      }
+
+      return acc;
+    }, [])
+    .join(", ");
+
+  console.log(parsedArgs);
 };
 
 parseArgs();
