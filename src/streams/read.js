9b45dfd37d764d5c42f943380734cf6353105172
Ivan Tsenilov <arantirex@gmail.com>
2024-10-08 00:47:32 +0400

    feat: implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import { open } from "node:fs/promises";
+import path from "path";
+import { getCurrentDir } from "../utils/utils.js";
+
 const read = async () => {
-    // Write your code here 
+  const { __dirname } = getCurrentDir(import.meta.url);
+  const filePath = path.join(__dirname, "files", "fileToRead.txt");
+
+  const fd = await open(filePath);
+  const stream = fd.createReadStream();
+
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  stream.on("end", () => {
+    console.log("");
+  });
+
+  stream.on("error", (err) => {
+    console.error(err);
+  });
 };
 
 await read();
